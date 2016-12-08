d8cebb8b4138b9ea02186405d9ec5ab83082a62a
Josh Coles <josh@joshcoles.com>
2016-12-08 04:51:18 +0000

    Made it so that administrators are able to create categories. Changed all MVC components to do so.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,9 @@
 class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.order(name: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
 end
