8e71e39f99b1dbc4ca4ac2de2c9662577e3b4379
Josh Coles <josh@joshcoles.com>
2016-12-08 20:23:34 +0000

    Finished creating admin categories funcitonality.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,13 +1,29 @@
 class Admin::CategoriesController < ApplicationController
-  def index
+
+ def index
     @categories = Category.order(name: :desc).all
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: "Your category has been created."
+    else
+      render :new
+    end
+
+  end
+
+  def new
     @category = Category.new
   end
 
   def show
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,4 +5,8 @@ class CategoriesController < ApplicationController
     @products = @category.products.order(created_at: :desc)
   end
 
+  def new
+    @category = Category.new
+  end
+
 end
