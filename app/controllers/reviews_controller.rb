c7f81f38323ab4fbc8f02c7b915b4b61739565d2
Josh Coles <josh@joshcoles.com>
2016-12-10 21:57:34 +0000

    Completed review feature, including ability for users to delete reviews.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
 
+  before_action :authorize
+
   def create
    @product = Product.find(params[:product_id])
    @review = @product.reviews.create(review_params)
@@ -14,7 +16,13 @@ class ReviewsController < ApplicationController
    end
  end
 
+  def destroy
+    Review.find(params[:id]).destroy
+    redirect_to product_path(params[:product_id])
+  end
+
  private
+
  def review_params
    params.require(:review).permit(:description, :rating)
  end
